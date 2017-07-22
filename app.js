2c9259dbed5b6ec5ea9f5b8aa6526a3644dc1703
VladWulf <vladimir@appliedblockchain.com>
2017-07-22 15:03:40 +0100

    more routes

--- app.js
+++ app.js
@@ -16,6 +16,32 @@ app.get('/', (req, res) =>{
     res.send('Hello World')
 })
 
+app.get('/insured/submit_policy', (req, res) => {
+    // insured submits a policy
+})
+
+app.get('/insured/accept_policy', (req, res) => {
+    // insured submits a price after valuation of policy
+})
+
+app.get('/insurer/submit_price', (req, res) => {
+    // insured accepts the policy
+})
+
+app.get('/insurer/action', (req, res) => {
+    // insurer accepts, denies or sumbits to review a given claim
+})
+
+app.get('/expert/action', (req, res) => {
+    // expert begins investigation or declines
+})
+
+app.get('/claimant/make_claim', (req, res) => {
+    // claimant makes a claim
+})
+
+
+
 
 app.listen(PORT, HOST, (req, res) => {
     console.log(`listening from ${HOST}:${PORT}`)
